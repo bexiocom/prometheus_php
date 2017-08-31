3319e58e7a78ae8a8e0feb993ae96a3557e82702
Christian Haeusler <christian.haeusler@bexio.com>
2017-08-31 14:29:12 +0200

    Histogram tests for InMemory storage

--- src/Storage/InMemory.php
+++ src/Storage/InMemory.php
@@ -6,6 +6,8 @@
 
 namespace Bexio\PrometheusPHP\Storage;
 
+use Bexio\PrometheusPHP\Metric\Histogram;
+use Bexio\PrometheusPHP\Metric\HistogramOptions;
 use Bexio\PrometheusPHP\MetricTypeCollection;
 use Bexio\PrometheusPHP\Sample;
 use Bexio\PrometheusPHP\Type\Addable;
@@ -13,6 +15,7 @@ use Bexio\PrometheusPHP\Type\Decrementable;
 use Bexio\PrometheusPHP\Type\Incrementable;
 use Bexio\PrometheusPHP\MetricType;
 use Bexio\PrometheusPHP\Options;
+use Bexio\PrometheusPHP\Type\Observable;
 use Bexio\PrometheusPHP\Type\Settable;
 use Bexio\PrometheusPHP\StorageAdapter;
 use Bexio\PrometheusPHP\Type\Subtractable;
@@ -94,6 +97,9 @@ class InMemory implements StorageAdapter
         });
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function set(Settable $metric, $value)
     {
         $this->withIndex($metric, function ($name, $labels) use ($value) {
@@ -101,10 +107,25 @@ class InMemory implements StorageAdapter
         });
     }
 
-    public function withIndex(MetricType $metric, callable $function)
+    /**
+     * {@inheritdoc}
+     */
+    public function observe(Observable $metric, $value)
+    {
+        $this->withIndex($metric, function ($name, $labels) {
+            $this->data[$name][$labels]++;
+        }, $value);
+    }
+
+    /**
+     * @param MetricType $metric
+     * @param callable   $function
+     * @param float|null $value
+     */
+    protected function withIndex(MetricType $metric, callable $function, $value = null)
     {
         $name = $metric->getOptions()->getFullyQualifiedName();
-        $labels = $this->getLabelsKey($metric->getOptions());
+        $labels = $this->getLabelsKey($metric->getOptions(), $value);
         $this->ensureIndex($name, $labels);
         $function($name, $labels);
     }
@@ -123,16 +144,28 @@ class InMemory implements StorageAdapter
     /**
      * Gets a unique identifier for the given options.
      *
-     * @param Options $options
+     * @param Options    $options
+     * @param float|null $value
      *
      * @return string
      */
-    private function getLabelsKey(Options $options)
+    private function getLabelsKey(Options $options, $value = null)
     {
         $labels = $options->getLabels();
+        if ($options instanceof HistogramOptions) {
+            $buckets = $options->getBuckets();
+            $bucket = '+Inf';
+            foreach ($buckets as $le) {
+                if ($value <= $le) {
+                    $bucket = $le;
+                    break;
+                }
+            }
+            $labels = array_merge($labels, array('le' => $bucket));
+        }
         ksort($labels);
 
-        return empty($labels) ? 'default' : json_encode($labels);
+        return empty($labels) ? InMemory::DEFAULT_VALUE_INDEX : json_encode($labels);
     }
 
     /**
@@ -157,7 +190,7 @@ class InMemory implements StorageAdapter
         $name = $metric->getOptions()->getFullyQualifiedName();
         if (isset($this->data[$name])) {
             foreach ($this->data[$name] as $key => $value) {
-                $options = 'default' == $key ?
+                $options = InMemory::DEFAULT_VALUE_INDEX == $key ?
                     $metric->getOptions()
                     : $metric->withLabels(json_decode($key, true))->getOptions();
                 $result[] = Sample::createFromOptions($options, $value);
@@ -174,12 +207,24 @@ class InMemory implements StorageAdapter
      */
     private function collectSingleSamples(MetricType $metric)
     {
-        $name = $metric->getOptions()->getFullyQualifiedName();
-        $labels = $this->getLabelsKey($metric->getOptions());
-        $value = isset($this->data[$name][$labels])
-            ? $this->data[$name][$labels]
-            : null;
+        $options = $metric->getOptions();
+        $name = $options->getFullyQualifiedName();
+        $buckets = $options instanceof HistogramOptions
+            ? array_merge($options->getBuckets(), array(PHP_INT_MAX))
+            : array(null);
+        $samples = array();
+        $sum = null;
+        foreach ($buckets as $le) {
+            $labelsKey = $this->getLabelsKey($options, $le);
+            $value = isset($this->data[$name][$labelsKey])
+                ? $this->data[$name][$labelsKey]
+                : null;
 
-        return array(Sample::createFromOptions($metric->getOptions(), $value));
+            $sum += $value;
+
+            $samples[] = Sample::createFromOptions($options, $options instanceof HistogramOptions ? $sum : $value);
+        }
+
+        return $samples;
     }
 }
